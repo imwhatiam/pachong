048bd47b363429b26e2baffdfed5164b9dd74332
lian <lian@seafile.com>
2021-03-16 16:16:51 +0800

    update

--- pachong.py
+++ pachong.py
@@ -1,8 +1,34 @@
-import re
+import os
 import time
+import sqlite3
 import requests
+from bs4 import BeautifulSoup
+# import re
+# from pprint import pprint
 
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36'
+db_name = 'result.db'
+
+if not os.path.exists(db_name):
+    con = sqlite3.connect(db_name)
+    cur = con.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS topic (
+            topic_url TEXT PRIMARY KEY,
+            title TEXT,
+            count INTEGER DEFAULT 0,
+            last_reply TEXT DEFAULT '',
+            create_time TEXT DEFAULT '',
+            content TEXT DEFAULT '',
+            image_url TEXT DEFAULT '');
+        ''')
+    con.commit()
+else:
+    con = sqlite3.connect(db_name)
+    cur = con.cursor()
+
+
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.3'
+
 headers = {
     'User-Agent': user_agent
 }
@@ -10,28 +36,62 @@ headers = {
 group_id = '628580'
 start = 0
 
-while start < 500:
-    time.sleep(5)
-    url = "https://www.douban.com/group/{}/discussion?start={}".format(group_id, start)
-    start += 25
+with open('result.md', 'w') as f:
 
-    r = requests.get(url, headers=headers)
+    f.write('|title|count|last reply|\n')
+    f.write('|-----|-----|----------|\n')
 
-    file_name = 'discussion?start={}.html'.format(start)
+    while start < 500:
 
-    with open(file_name, 'w') as f:
-        f.write(r.text)
+        time.sleep(5)
+        url = "https://www.douban.com/group/{}/discussion?start={}".format(group_id, start)
+        start += 25
+        print(url)
 
-    lines = []
-    with open(file_name, 'r') as f:
-        for line in f.readlines():
-            if 'https://www.douban.com/group/topic/' in line:
-                lines.append(line.strip())
+        r = requests.get(url, headers=headers)
+        soup = BeautifulSoup(r.text, 'html.parser')
 
-    regex = '.*href="(.*)" title="(.*)" class.*'
+        table = soup.find('table', attrs={'class': 'olt'})
+        if not table:
+            continue
 
-    with open('result.md', 'a') as f:
-        for line in lines:
-            match = re.match(regex, line)
-            if match:
-                f.write('[{}]({})'.format(match.group(2), match.group(1)) + '\n\n')
+        rows = table.find_all('tr')
+        if not rows:
+            continue
+
+        for row in rows:
+
+            if row.find('span', attrs={'class': 'elite_topic_lable'}):
+
+                a = row.find('a', href=True)
+                topic_url = a['href']
+                title = a['title']
+                count = row.find('td', attrs={'class': 'r-count'}).text
+                last_reply = row.find('td', attrs={'class': 'time'}).text
+
+                if '193662234' in topic_url:
+                    continue
+
+                select_sql = "SELECT * FROM topic WHERE topic_url='{}'".format(topic_url)
+                cur.execute(select_sql)
+                if len(cur.fetchall()) > 0:
+                    continue
+
+                insert_sql = "INSERT INTO topic VALUES ('{}','{}',{},'{}','','','');".format(topic_url,
+                                                                                             title,
+                                                                                             count or 0,
+                                                                                             last_reply)
+                try:
+                    cur.execute(insert_sql)
+
+                    f.write('|[{}]({})|{}|{}|\n'.format(title.replace('|', ','),
+                                                        topic_url,
+                                                        count or 0,
+                                                        last_reply))
+
+                except Exception as e:
+                    print(e)
+                    print(insert_sql)
+
+con.commit()
+con.close()
